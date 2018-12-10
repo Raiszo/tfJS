21eb03002979778e009ee7ddaabbddb3aacd05d0
Raiszo <E05.sek@gmail.com>
2018-12-10 13:02:27 -0500

    :dagger: loading works :v

---
+++ main.js
@@ -0,0 +1,13 @@
+const tf = require('@tensorflow/tfjs')
+require('@tensorflow/tfjs-node')
+
+async function main() {
+	const model = await tf.loadModel('file://./models/model.json');
+
+	console.log(model)
+}
+
+main()
+	.then(() => console.log('alles gut'))
+	.catch(e => console.log(e))
+	.finally(() => console.log('termine :v'))
--- test1.js
+++
@@ -1,6 +0,0 @@
-import * as tf from '@tensorflow/tfjs'
-
-let t = tf.tensor([[5,6,7,8],[1,2,3,4]])
-
-t.print()
-console.log(t)
